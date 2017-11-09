aed972b8004eb399959e4877d2b563cf276717af
Matthew Dworkin <mdworkin21@gmail.com>
2017-11-09 03:06:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+function scuberGreetingForFeet(d){
+  var d;
+  if(d < 400){
+    return "This one is on me!"
+  }
+  else if(d > 2000 && d < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else{
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city){
+
+  city === "NYC" ? "'Ok, sounds good.": "No go."
+}
